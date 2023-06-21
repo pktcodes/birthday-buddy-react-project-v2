fc557f9187a9de6e19631f8ceaac4371898be9e2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-20 22:31:12 -0500

    Import List

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
+  const [people, setPeople] = useState(data);
+  console.log(people);
+
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;
