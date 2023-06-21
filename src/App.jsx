6458fa6e3431217589aae5de3fa5b28c10f220d4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-20 23:18:57 -0500

    Render List

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import data from './data';
+import List from './List';
 
 const App = () => {
   const [people, setPeople] = useState(data);
-  console.log(people);
 
-  return <h2>Birthday Reminder - Starter</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+      </section>
+    </main>
+  );
 };
 export default App;
