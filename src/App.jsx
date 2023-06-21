e223b61a25d404cda963af491330441bb3a5abd1
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-20 23:42:22 -0500

    Clear List

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ const App = () => {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
       </section>
     </main>
   );
